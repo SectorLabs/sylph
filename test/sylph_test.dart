b3ea6e9c66fab4de9dae7c575babcc77ae1f3ae4
Omar Iqbal Naru <omar.iqbal@zameen.com>
2021-06-02 15:49:51 +0500

    Revert "Update sylph_test.dart"
    This reverts commit 76ac1342a0bc032d458ab8080987dce7e55e2089.

--- test/sylph_test.dart
+++ test/sylph_test.dart
@@ -376,7 +376,7 @@ void main() {
         tmp_dir: /tmp/sylph
         artifacts_dir: /tmp/sylph_artifacts
         sylph_timeout: 720 
-        flavor: staging
+        flavor: dev
         concurrent_runs: true
         project_name: test concurrent runs
         default_job_timeout: 10 
